939969f1b76585db73c3964762d3d79edfe84ca0
bbastianb <ba.burgose@duocuc.cl>
2025-10-20 19:41:31 -0300

    Implemento la validacion de login ademas de retoques

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import ListadoProd from "./pages/ListadoProd.jsx";
+import Registro from "./pages/Registro.jsx";
+import HomeAdmin from "./pages/admin/HomeAdmin.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,7 +15,9 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: "login", element: <Login /> },
-      {path:"/productos", element:<ListadoProd/>}
+      { path: "productos", element: <ListadoProd /> },
+      { path: "registro", element: <Registro /> },
+      { path: "admin", element: <HomeAdmin /> },
     ],
   },
 ]);
